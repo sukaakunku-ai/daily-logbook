7a1def7d80ba61a66ca59e4be701dc8f69267e36
muhamadiruel <86477252+muhamadiruel@users.noreply.github.com>
2026-02-06 10:39:22 +0700

    Cleanup .env and use environment variables for Firebase configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGyk2L2RSvizIr_aXKyX44vWPlyM8rMFk",
-  authDomain: "pengeluaran-dd4d0.firebaseapp.com",
-  projectId: "pengeluaran-dd4d0",
-  storageBucket: "pengeluaran-dd4d0.firebasestorage.app",
-  messagingSenderId: "254908773545",
-  appId: "1:254908773545:web:c52a2a33b081b83e6425b8",
-  measurementId: "G-44QZ1165HM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
